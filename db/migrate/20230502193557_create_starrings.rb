8c3f13d8abddde16a2bacff3dace2fcb2acafa0a
Huong Vu <huong.vu@kellogg.northwestern.edu>
2023-05-02 19:52:54 +0000

    Commit please

--- db/migrate/20230502193557_create_starrings.rb
+++ db/migrate/20230502193557_create_starrings.rb
@@ -1,7 +1,9 @@
 class CreateStarrings < ActiveRecord::Migration[7.0]
   def change
     create_table :starrings do |t|
-
+      t.integer "actor_id"
+      t.integer "movie_id"
+      t.string "character"
       t.timestamps
     end
   end
