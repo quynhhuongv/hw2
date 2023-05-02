6308b7ad2bb961da2f4ea1b79f972521f49095a8
Huong Vu <huong.vu@kellogg.northwestern.edu>
2023-05-02 20:36:58 +0000

    Commit please

--- kmdb.rb
+++ kmdb.rb
@@ -88,14 +88,75 @@ Starring.destroy_all
 # Do not use hard-coded foreign key IDs.
 # TODO!
 studio_1 = Studio.new
-studio_1["studio_id"] = "Warner Bros"
+studio_1["name"] = "Warner Bros"
 studio_1.save
 
 actor_1 = Actor.new
-actor_1["actor_id"] = "Christian Bale"
+actor_1["name"] = "Christian Bale"
 actor_1.save
 
+actor_2 = Actor.new
+actor_2["name"] = "Michael Caine"
+actor_2.save
 
+actor_3 = Actor.new
+actor_3["name"] = "Liam Neeson"
+actor_3.save
+
+actor_4 = Actor.new
+actor_4["name"] = "Katie Holmes"
+actor_4.save
+
+actor_5 = Actor.new
+actor_5["name"] = "Gary Oldman"
+actor_5.save
+
+actor_6 = Actor.new
+actor_6["name"] = "Heath Ledger"
+actor_6.save
+
+actor_7 = Actor.new
+actor_7["name"] = "Aaron Eckhart"
+actor_7.save
+
+actor_8 = Actor.new
+actor_8["name"] = "Maggie Gyllenhaal"
+actor_8.save
+
+actor_9 = Actor.new
+actor_9["name"] = "Tom Hardy"
+actor_9.save
+
+actor_10 = Actor.new
+actor_10["name"] = "Joseph Gordon-Levitt"
+actor_10.save
+
+actor_11 = Actor.new
+actor_11["name"] = "Anne Hathaway"
+actor_11.save
+
+warner = Studio.find_by({ "name" => "Warner Bros" })
+
+movie_1 = Movie.new
+movie_1["title"] = "Batman Begins"
+movie_1["year"] = "2005"
+movie_1["mpaa_rating"] = "PG-13"
+movie_1["studio_id"] = warner["id"]
+movie_1.save
+
+movie_2 = Movie.new
+movie_2["title"] = "The Dark Knight"
+movie_2["year"] = "2008"
+movie_2["mpaa_rating"] = "PG-13"
+movie_2["studio_id"] = warner["id"]
+movie_2.save
+
+movie_3 = Movie.new
+movie_3["title"] = "The Dark Knight Rises"
+movie_3["year"] = "2012"
+movie_3["mpaa_rating"] = "PG-13"
+movie_3["studio_id"] = warner["id"]
+movie_3.save
 
 # Prints a header for the movies output
 puts "Movies"
